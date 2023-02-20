bb1686cfa27b78499a21a1c36f69f3cbbe72152d
František Poboček <pobocekfrantisek@gmail.com>
2023-02-20 21:20:57 +0100

    Events example with increment & test

--- src/lib.rs
+++ src/lib.rs
@@ -10,5 +10,24 @@ impl Contract {
     }
 }
 
+const COUNTER: Symbol = symbol!("COUNTER");
+
+pub struct IncrementContract;
+
+#[contractimpl]
+impl IncrementContract {
+    pub fn increment(env: Env) -> u32 {
+        let mut count: u32 = env
+            .storage()
+            .get(&COUNTER)
+            .unwrap_or(Ok(0)) // If no value set, assume 0.
+            .unwrap(); // Panic if the value of COUNTER is not u32.
+        count += 1;
+        env.storage().set(&COUNTER, &count);
+        env.events().publish((COUNTER, symbol!("increment")), count);
+        count
+    }
+}
+
 #[cfg(test)]
 mod test;
--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,8 @@
 #![cfg(test)]
+use crate::{IncrementContract, IncrementContractClient};
+
 use super::{Contract, ContractClient};
-use soroban_sdk::{symbol, vec, Env};
+use soroban_sdk::{symbol, testutils::Events, vec, Env, IntoVal};
 
 #[test]
 fn test() {
@@ -10,4 +12,37 @@ fn test() {
 
     let words = client.hello(&symbol!("Dev"));
     assert_eq!(words, vec![&env, symbol!("Hello"), symbol!("Dev"),]);
+}
+
+#[test]
+fn test_event() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, IncrementContract);
+    let client = IncrementContractClient::new(&env, &contract_id);
+
+    assert_eq!(client.increment(), 1);
+    assert_eq!(client.increment(), 2);
+    assert_eq!(client.increment(), 3);
+
+    assert_eq!(
+        env.events().all(),
+        vec![
+            &env,
+            (
+                contract_id.clone(),
+                (symbol!("COUNTER"), symbol!("increment")).into_val(&env),
+                1u32.into_val(&env)
+            ),
+            (
+                contract_id.clone(),
+                (symbol!("COUNTER"), symbol!("increment")).into_val(&env),
+                2u32.into_val(&env)
+            ),
+            (
+                contract_id.clone(),
+                (symbol!("COUNTER"), symbol!("increment")).into_val(&env),
+                3u32.into_val(&env)
+            ),
+        ]
+    );
 }
